398dc84453551e5e7ff4f3f479fd1b75fc9c9ca9
picarenlamina <116097495+picarenlamina@users.noreply.github.com>
2026-01-08 16:09:39 +0100

    Update Calculator.php

--- src/Helper/Calculator.php
+++ src/Helper/Calculator.php
@@ -1,12 +1,12 @@
 <?php
 namespace App\Helper;
+
 class Calculator
 {
     public function sum(float $a, float $b): float
     {
         return $a + $b;
     }
-
     public function div(float $a, float $b): float
     {
         return $a / $b;
